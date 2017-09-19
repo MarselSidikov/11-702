3f49026956b575dc3af1d85f717114330746ca7e
Marsel Sidikov <sidikov.marsel@gmail.com>
2017-09-19 13:18:05 +0300

    add task11

---
+++ FunctionsAndProcedures/src/ru/itis/Program3.java
@@ -0,0 +1,29 @@
+package ru.itis;
+
+/**
+ * 19.09.2017
+ * Program3
+ *
+ * @author Sidikov Marsel (First Software Engineering Platform)
+ * @version v1.0
+ */
+public class Program3 {
+    public static double function(double x) {
+        return Math.sin(1 / x);
+    }
+    public static double integrate(double a, double b, double h) {
+        //
+        return 0;
+    }
+    public static void main(String[] args) {
+        // считать
+        double a = 0;
+        double b = 0;
+        int n;
+
+        // посчитать
+        double h = 0;
+
+        double integral = integrate(a, b, h);
+    }
+}
