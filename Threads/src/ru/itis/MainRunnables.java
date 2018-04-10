69de25dd1b41ca88c4d38fed72fd290a44ef79a8
MarselSidikov <sidikov.marsel@gmail.com>
2018-04-10 14:23:03 +0300

    add threads

---
+++ Threads/src/ru/itis/MainRunnables.java
@@ -0,0 +1,11 @@
+package ru.itis;
+
+/**
+ * 10.04.2018
+ * MainRunnables
+ *
+ * @author Sidikov Marsel (First Software Engineering Platform)
+ * @version v1.0
+ */
+public class MainRunnables {
+}
